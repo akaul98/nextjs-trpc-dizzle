775fb0fdf342d30573cab656b3c6ce5bd4e5d208
Adarsh Kaul <ak.adarshkaul@gmail.com>
2025-06-24 00:10:59 +0530

    feat: implement OTP login functionality in the login form

--- src/components/login-form.tsx
+++ src/components/login-form.tsx
@@ -1,39 +1,51 @@
+"use client";
 import { cn } from "@/lib/utils"
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
+import { useState } from "react";
 
 export function LoginForm({
   className,
   ...props
 }: React.ComponentProps<"form">) {
+  const [isOtp, setIsOtp] = useState(false);
+  async function onClickSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    setIsOtp(true);
+  }
   return (
-    <form className={cn("flex flex-col gap-6", className)} {...props}>
+    <form className={cn("flex flex-col gap-6", className)} {...props} onSubmit={onClickSubmit}>
       <div className="flex flex-col items-center gap-2 text-center">
         <h1 className="text-2xl font-bold">Login to your account</h1>
         <p className="text-muted-foreground text-sm text-balance">
-          Enter your email below to login to your account
+          Enter your Phone and OrgCode below to login to your account
         </p>
       </div>
       <div className="grid gap-6">
         <div className="grid gap-3">
-          <Label htmlFor="email">Email</Label>
-          <Input id="email" type="email" placeholder="m@example.com" required />
+          <Label htmlFor="phone">Phone</Label>
+          <Input id="phone" type="phone" placeholder="9764XXXXXX" required />
         </div>
         <div className="grid gap-3">
           <div className="flex items-center">
-            <Label htmlFor="password">Password</Label>
-            <a
-              href="#"
-              className="ml-auto text-sm underline-offset-4 hover:underline"
-            >
-              Forgot your password?
-            </a>
+            <Label htmlFor="orgCode">OrgCode</Label>
           </div>
-          <Input id="password" type="password" required />
+          <Input id="orgCode" type="text" required />
         </div>
-        <Button type="submit" className="w-full">
-          Login
+        {isOtp&&
+        <div className="grid gap-3">
+          <div className="flex items-center">
+            <Label htmlFor="otp">OTP</Label>
+            </div>
+            <Input id="otp" type="text" placeholder="Enter OTP" required />
+            </div>}
+        <Button type="submit" className="w-full"
+        >
+          {isOtp?
+            "Login with OTP"
+            :"Submit"}
+            
         </Button>
         <div className="after:border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t">
           <span className="bg-background text-muted-foreground relative z-10 px-2">
